74e016312f90a8c7860c05bd280c9ee09301f1b9
Bishal Roy <bishalroy895@gmail.com>
2024-04-13 12:14:34 +0530

    refactor

--- cmd/root.go
+++ cmd/root.go
@@ -10,12 +10,6 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "email-validify",
 	Short: "A cli tool to validate an email address",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 }
 
 func Execute() {
